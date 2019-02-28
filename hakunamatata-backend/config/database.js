6dc20dc2436e1c71d8de822c2db6c027353b73ba
ujstha <ujstha27@gmail.com>
2019-02-28 13:17:34 +0200

    required modules installed

--- hakunamatata-backend/config/database.js
+++ hakunamatata-backend/config/database.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: 'mongodb://hakunamatata:vforvendetta1@ds155665.mlab.com:55665/hakunamatata'
+    url: 'mongodb://hakunamatata:vforvendetta1@ds155665.mlab.com:55665/hakunamatata',
+    secret: ''
 };
